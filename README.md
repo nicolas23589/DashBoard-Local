378d183808d2d1c6357a6f22fb3141e51bc48e7d
Nicolás23589 <n.murilloc@uniandes.edu.co>
2024-07-28 17:49:03 -0400

    start commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DashBoardElectricity
+# DashboradTest
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.10.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DashBoardElectricity'`, () => {
+  it(`should have as title 'DashboradTest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DashBoardElectricity');
+    expect(app.title).toEqual('DashboradTest');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('DashBoardElectricity app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('DashboradTest app is running!');
   });
 });
